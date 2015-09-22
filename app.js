1bb17b8a2a5249b3abb1a5839b43155386e57467
Zack Elias <zelias500@gmail.com>
2015-09-22 17:33:38 -0400

    configured express

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 var express = require('express');
 var app = express();
 
+app.use(function(req, res, next){
+	console.log(req.method);
+	console.log(req.url);
+	console.log(res.statusCode);
+	console.log("==============================")
+	next();
+})
+
+
 app.get('/', function(request, response) {
 	response.send("Hello, world!");
+})
+
+app.get('/about', function(request, response ){
+	response.send("What are you talking about??");
 })
 
 app.listen(8080);
